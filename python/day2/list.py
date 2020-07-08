921c78a18a27b69ec7ca28ee8668d5c28b3ac599
HaeInn Kim <skyv2080@gmail.com>
2020-07-08 17:58:23 +0900

    day2 chapter 8

--- python/day2/list.py
+++ python/day2/list.py
@@ -1,5 +1,13 @@
-colors = ['red','blue','green']
-print(colors[0])
-print(colors[1])
-print(colors[2])
-print(len(colors))
+# colors = ['red','blue','green']
+# print(colors[0])
+# print(colors[1])
+# print(colors[2])
+# print(len(colors))
+
+cities = ['서울','도쿄','뉴욕','북경','런던','파리','시드니','방콕']
+print(cities[0:4])
+print(cities[1:6])
+print(cities[5:])
+last3_cities = cities[-3:]
+print(cities[-3:])
+
